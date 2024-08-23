4105abf4dfcd204b212e1d90c7c13074371d112b
JoaoCarlosP <joaocarlosjoe74@gmail.com>
2024-08-22 23:09:03 -0300

    feat: create a front end project and added routes, pages, api config and more

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
+import { HttpsRedirect } from './providers/HttpsRedirect'
+import RootRoutes from './routes'
+
+import './styles/index.scss'
+
+function App() {
+  return (
+    <BrowserRouter>    
+      <HttpsRedirect>
+        <RootRoutes />
+      </HttpsRedirect>
+    </BrowserRouter>
+  )
+}
+
+export default App
